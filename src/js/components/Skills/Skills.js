1ebb638ede04ac6b1379533334efbc334b738480
Aman Jain <91aman@gmail.com>
2017-08-15 11:45:21 +0530

    Fixed experience.

--- src/js/components/Skills/Skills.js
+++ src/js/components/Skills/Skills.js
@@ -1,103 +1,103 @@
 export default {
-    React: {
-        label: 'React',
-        logo: 'src/img/technology/React.png',
-        url: 'https://facebook.github.io/react/'
-    },
-    javascript: {
-        label: 'Javascript',
-        logo: 'src/img/technology/javascript.png',
-        url: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript'
-    },
-    css3: {
-        label: 'CSS3',
-        logo: 'src/img/technology/css3.png',
-        url: 'https://developer.mozilla.org/en-US/docs/Web/CSS/CSS3'
-    },
-    html5: {
-        label: 'HTML5',
-        logo: 'src/img/technology/html5.png',
-        url: 'https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5'
-    },
-    backbone: {
-        label: 'Backbone',
-        logo: 'src/img/technology/backbone.png',
-        url: 'http://backbonejs.org/'
-    },
-    jQuery: {
-        label: 'jQuery',
-        logo: 'src/img/technology/jquery.png',
-        url: 'https://jquery.com/'
-    },
-    lodash: {
-        label: 'lodash',
-        logo: 'src/img/technology/lodash.png',
-        url: 'https://lodash.com/'
-    },
-    webpack: {
-        label: 'Webpack',
-        logo: 'src/img/technology/webpack.png',
-        url: 'https://webpack.github.io/'
-    },
-    requireJs: {
-        label: 'requirejs',
-        logo: 'src/img/technology/requirejs.png',
-        url: 'http://requirejs.org/'
-    },
-    bootstrap: {
-        label: 'Bootstrap',
-        logo: 'src/img/technology/bootstrap.png',
-        url: 'http://getbootstrap.com/'
-    },
-    less: {
-        label: 'Less',
-        logo: 'src/img/technology/less.png',
-        url: 'http://lesscss.org/'
-    },
-    redux: {
-        label: 'Redux',
-        logo: 'src/img/technology/redux.png',
-        url: 'http://redux.js.org/'
-    },
-    d3: {
-        label: 'D3',
-        logo: 'src/img/technology/D3.png',
-        url: 'https://d3js.org/'
-    },
-    highCharts: {
-        label: 'High Charts',
-        logo: 'src/img/technology/highCharts.png',
-        url: 'http://www.highcharts.com/'
-    },
-    gulp: {
-        label: 'Gulp',
-        logo: 'src/img/technology/gulp.png',
-        url: 'http://gulpjs.com/'
-    },
-    git: {
-        label: 'Git',
-        logo: 'src/img/technology/git.png',
-        url: 'https://git-scm.com/'
-    },
-    intellij: {
-        label: 'Intellij',
-        logo: 'src/img/technology/intellij.jpeg',
-        url: 'https://www.jetbrains.com/idea/'
-    },
-    npm: {
-        label: 'npm',
-        logo: 'src/img/technology/npm.png',
-        url: 'https://www.npmjs.com/'
-    },
-    node: {
-        label: 'nodejs',
-        logo: 'src/img/technology/node.png',
-        url: 'https://nodejs.org/en/'
-    },
-    svn: {
-        label: 'subversion',
-        logo: 'src/img/technology/subversion.png',
-        url: 'https://subversion.apache.org/'
-    }
+  React: {
+    label: 'React',
+    logo: 'src/img/technology/React.png',
+    url: 'https://facebook.github.io/react/'
+  },
+  javascript: {
+    label: 'Javascript',
+    logo: 'src/img/technology/javascript.png',
+    url: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript'
+  },
+  css3: {
+    label: 'CSS3',
+    logo: 'src/img/technology/css3.png',
+    url: 'https://developer.mozilla.org/en-US/docs/Web/CSS/CSS3'
+  },
+  html5: {
+    label: 'HTML5',
+    logo: 'src/img/technology/html5.png',
+    url: 'https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5'
+  },
+  redux: {
+    label: 'Redux',
+    logo: 'src/img/technology/redux.png',
+    url: 'http://redux.js.org/'
+  },
+  backbone: {
+    label: 'Backbone',
+    logo: 'src/img/technology/backbone.png',
+    url: 'http://backbonejs.org/'
+  },
+  jQuery: {
+    label: 'jQuery',
+    logo: 'src/img/technology/jquery.png',
+    url: 'https://jquery.com/'
+  },
+  lodash: {
+    label: 'lodash',
+    logo: 'src/img/technology/lodash.png',
+    url: 'https://lodash.com/'
+  },
+  webpack: {
+    label: 'Webpack',
+    logo: 'src/img/technology/webpack.png',
+    url: 'https://webpack.github.io/'
+  },
+  requireJs: {
+    label: 'requirejs',
+    logo: 'src/img/technology/requirejs.png',
+    url: 'http://requirejs.org/'
+  },
+  bootstrap: {
+    label: 'Bootstrap',
+    logo: 'src/img/technology/bootstrap.png',
+    url: 'http://getbootstrap.com/'
+  },
+  scss: {
+    label: 'Scss',
+    logo: 'src/img/technology/scss.png',
+    url: 'http://sass-lang.com/'
+  },
+  d3: {
+    label: 'D3',
+    logo: 'src/img/technology/D3.png',
+    url: 'https://d3js.org/'
+  },
+  highCharts: {
+    label: 'High Charts',
+    logo: 'src/img/technology/highCharts.png',
+    url: 'http://www.highcharts.com/'
+  },
+  gulp: {
+    label: 'Gulp',
+    logo: 'src/img/technology/gulp.png',
+    url: 'http://gulpjs.com/'
+  },
+  git: {
+    label: 'Git',
+    logo: 'src/img/technology/git.png',
+    url: 'https://git-scm.com/'
+  },
+  intellij: {
+    label: 'Intellij',
+    logo: 'src/img/technology/intellij.jpeg',
+    url: 'https://www.jetbrains.com/idea/'
+  },
+  npm: {
+    label: 'npm',
+    logo: 'src/img/technology/npm.png',
+    url: 'https://www.npmjs.com/'
+  },
+  node: {
+    label: 'nodejs',
+    logo: 'src/img/technology/node.png',
+    url: 'https://nodejs.org/en/'
+  },
+  svn: {
+    label: 'subversion',
+    logo: 'src/img/technology/subversion.png',
+    url: 'https://subversion.apache.org/'
+  }
 
 };
